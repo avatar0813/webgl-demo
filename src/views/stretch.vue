477e5258e2c49d878364962a1169ff0f91b13adf
avatar <oooooooyk0813@gmail.com>
2023-11-30 10:38:28 +0800

    feat: 拉伸几何体

--- src/views/stretch.vue
+++ src/views/stretch.vue
@@ -8,7 +8,6 @@
 <script setup>
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-import { VertexNormalsHelper } from 'three/addons/helpers/VertexNormalsHelper.js';
 
 import { onMounted, ref } from 'vue'
 
@@ -24,14 +23,14 @@ const initScene = () => {
   const scene = new THREE.Scene()
   scene.background = new THREE.Color(0xb6d4ff)
   const axesHelper = new THREE.AxesHelper( 20 );
-  // const gridHelper = new THREE.GridHelper( 20, 20 );
   scene.add( axesHelper )
+  scene.rotateY(-2.2)
 
   // 2、相机 调整初始相机角度
-  const camera = new THREE.PerspectiveCamera(45, containerDom.clientWidth / containerDom.clientHeight, 1, 1000)
-  camera.position.z = 10;
-  camera.position.x = 2;
-  camera.position.y = 2;
+  const camera = new THREE.PerspectiveCamera(45, containerDom.clientWidth / containerDom.clientHeight, 1, 5000)
+  camera.position.z = 10
+  camera.position.x = 2
+  camera.position.y = 5
 
   // 3、添加几何图形
   // 顶点
@@ -54,11 +53,11 @@ const initScene = () => {
       3,5,0
 
   ]
-const geometry = new THREE.PolyhedronGeometry(verticesOfCube, indicesOfFaces, 2, 0)
+  const geometry = new THREE.PolyhedronGeometry(verticesOfCube, indicesOfFaces, 2, 0)
+  geometry.setIndex = new THREE.Uint16BufferAttribute(indicesOfFaces)
   const materials = new THREE.MeshMatcapMaterial( { color: 0xffffff, side: THREE.DoubleSide } )
   const cube = new THREE.Mesh( geometry, materials );
-  const normalsHelper = new VertexNormalsHelper( cube, 5, 0x00ff00, 5 )
-  scene.add(cube).add(normalsHelper)
+  scene.add(cube)
 
    // 4、渲染
   const renderer = new THREE.WebGLRenderer({ antialias: true })
@@ -67,58 +66,73 @@ const geometry = new THREE.PolyhedronGeometry(verticesOfCube, indicesOfFaces, 2,
   // 添加轨道控制器 使其可以拖动切换视角
   const orbitControl = new OrbitControls(camera, renderer.domElement);
 
-  // 5、添加射线hover face
+  // 5、添加射线 判断是否点击在几何体上
   const raycaster = new THREE.Raycaster()
   const pointer = new THREE.Vector2()
   let activeInCube = false
   let activeIntersect = null
 
-  // <-------->
-  function onPointerMove( ) {
+  function onPointerMove() {
     if (!activeInCube) return
+
     const face = activeIntersect.face
-    // 平面法向量
-    const planeNormal = activeIntersect.normal
-    console.log(face, planeNormal)
-    const indexA = new THREE.Vector3(
+    const planeNormal = activeIntersect.normal // 平面法向量
+    const verticeXYZArr = [] // 原面上的顶点坐标展开, verticeXYZArr中每三个元素表示一个顶点
+    const verticeIndexArr = [] // 顶点索引集合
+    const verticePointArr = [] // 订单三维坐标集合
+    const updatePointArr = [] // 更新后的顶点坐标
+    // 偏移量: 根据法向量长度(默认1) / 50 = 0.01
+    const scaleDirection = planeNormal.clone().divideScalar(50)
+
+    // 一个面有3个点，记录每个点的position信息(x, y, z)
+    const positionArr = [
       geometry.attributes.position.getX(face.a),
       geometry.attributes.position.getY(face.a),
       geometry.attributes.position.getZ(face.a),
-    )
-    const indexB = new THREE.Vector3(
       geometry.attributes.position.getX(face.b),
       geometry.attributes.position.getY(face.b),
       geometry.attributes.position.getZ(face.b),
-    )
-    const indexC = new THREE.Vector3(
       geometry.attributes.position.getX(face.c),
       geometry.attributes.position.getY(face.c),
       geometry.attributes.position.getZ(face.c),
-    )
+    ]
 
-    // 偏移量
-    const offsetValue = 1.01
-    console.log('index1:', indexA, indexB, indexC)
-    const scaleDirection = planeNormal.clone().multiplyScalar(offsetValue)
-    indexA.add(scaleDirection)
-    indexB.add(scaleDirection)
-    indexC.add(scaleDirection)
-    console.log('index2:', indexA, indexB, indexC)
-    const faceArr = [face.a, face.b, face.c]
-    const indexArr =  [indexA, indexB, indexC]
-    for (let i = 0; i < faceArr.length; i++) {
-      const vertice = faceArr[i]
-      const index = indexArr[i]
-      geometry.attributes.position.setXYZ(vertice, index.x, index.y, index.z)
+    // position.arr上找到位置
+    const geoPSArr = geometry.attributes.position.array
+    for (let i = 0; i < geoPSArr.length; i+=3) {
+      if (
+        positionArr[0] === geoPSArr[i] && positionArr[1] === geoPSArr[i + 1] && positionArr[2] === geoPSArr[i + 2]
+        || positionArr[3] === geoPSArr[i] && positionArr[4] === geoPSArr[i + 1] && positionArr[5] === geoPSArr[i + 2]
+        || positionArr[6] === geoPSArr[i] && positionArr[7] === geoPSArr[i + 1] && positionArr[8] === geoPSArr[i + 2]
+      ) {
+        verticeXYZArr.push(i, i + 1, i + 2)
+        verticeIndexArr.push(i / 3)
+      }
+    }
+
+    // 设置新旧顶点坐标
+    verticeIndexArr.forEach(vertice => {
+      const originPointer = new THREE.Vector3(
+        geometry.attributes.position.getX(vertice),
+        geometry.attributes.position.getY(vertice),
+        geometry.attributes.position.getZ(vertice),
+      )
+      verticePointArr.push(originPointer)
+      updatePointArr.push(originPointer.add(scaleDirection))
+    })
+
+    for (let i = 0; i < verticeIndexArr.length; i++) {
+      const vertice = verticeIndexArr[i]
+      const point = updatePointArr[i]
+      geometry.attributes.position.setXYZ(vertice, point.x, point.y, point.z)
     }
 
     geometry.attributes.position.needsUpdate = true
-    renderer.render( scene, camera )
-    normalsHelper.update()
   }
 
   // 6、添加拖放控制
   function onpointerDown(event) {
+
     const rect = containerDom.getBoundingClientRect()
     pointer.x = ( (event.clientX - rect.x) / renderer.domElement.clientWidth ) * 2 - 1;
     pointer.y = - ( (event.clientY - rect.y) / renderer.domElement.clientHeight ) * 2 + 1;
@@ -147,7 +161,6 @@ const geometry = new THREE.PolyhedronGeometry(verticesOfCube, indicesOfFaces, 2,
   function animate() {
     requestAnimationFrame(animate)
     orbitControl.update()
-    normalsHelper.update()
     renderer.render(  scene, camera );
   }
   animate()
